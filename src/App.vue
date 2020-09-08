3d3ae21f1603428f7c274149525ce5dc85d57780
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-09-08 17:42:21 +0300

    Repo initialized

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app">
+    <p class="text-success font-size-10">Todo App</p>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+
+/** Ant Design Component Library */
+import "@/components/Antd"
+
+/**
+ * 
+ * Project general css files.
+ * Change here if you want to
+ * change presets.
+ * 
+ */
+import "@/assets/styles/style.scss"
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app")
