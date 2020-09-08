6d15503a644dc36d52c083b7d192f3da27a4e420
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-09-08 22:05:26 +0300

    Project component created and implemented for structure

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -2,14 +2,22 @@
   <div style="height: 100vh">
     <div class="row h-100 gradient-dark p-3 m-0">
       <div class="col-xl-4">
-        <div class="row h-100">
-          <div class="col-xl-12 d-flex flex-column justify-content-center">
+        <div class="row h-100 d-flex justify-content-center">
+          <div class="col-xl-9 d-flex flex-column justify-content-center">
             <div>
-              <p class="mb-0 text-white font-size-32 font-weight-bold">Hi Mehmet</p>
-              <p class="mb-0 font-size-12 text-light" style="opacity: .5;">Welcome back to the workspace, we missed you!</p>
+              <p class="mb-0 text-white font-size-36 font-weight-bold">Hi Mehmet</p>
+              <p class="mb-0 font-size-14 text-light">Welcome back to the workspace, we missed you!</p>
             </div>
-            <div class="w-50 mt-4">
+            <div class="my-4">
               <search-bar></search-bar>
+            </div>
+            <div>
+              <p class="d-flex font-size-16 text-white mt-5">Projects <span class="font-size-14 text-light ml-2">(3)</span></p>
+              <div class="d-flex flex-wrap justify-content-between">
+                <div v-for="(item, key) in 6" :key="key" class="my-2">
+                  <project />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -17,7 +25,7 @@
       <div class="col-xl-8 bg-white" style="border-radius: 1rem;">
         <div class="row">
           <div class="col-xl-12 py-2">
-            asd
+            <p class="font-size-16 text-dark">asdsad</p>
           </div>
         </div>
       </div>
@@ -27,10 +35,9 @@
 
 <script>
 import SearchBar from "@/components/SearchBar"
+import Project from "@/components/Project"
 
 export default {
-  components: {
-    SearchBar
-  }
+  components: { SearchBar, Project }
 }
 </script>
