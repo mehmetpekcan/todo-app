918d622b01613c07730c7a602c2f45ffdc71d773
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-09-08 21:35:24 +0300

    Home layout design step forwarded, search component design done

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="search--input-wrapper d-flex align-items-center">
+    <i class="fas fa-search ml-3" />
+    <input type="text" class="search--input" placeholder="Search Task or Project..">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/mixins.scss";
+
+.search--input-wrapper {
+  background: $dark-gray;
+  border-radius: 1rem;
+
+  .fa-search {
+    width: 5%;
+    color: $light;
+    opacity: .7;
+    cursor: pointer;
+  }
+
+  .search--input {
+    width: 95%;
+    padding: .8rem .5rem;
+    border: 0;
+    color: $light;
+    font-size: .8rem;
+    opacity: .7;
+    outline: none;
+    background: none;
+
+    &::placeholder {
+      color: $light;
+    }
+  }
+}
+</style>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div style="height: 100vh">
-    <div class="row h-100 bg-dark p-2 m-0">
+    <div class="row h-100 gradient-dark p-3 m-0">
       <div class="col-xl-4">
-        <div class="row">
-          <div class="col-xl-12">
-            <p class="mb-0 text-white">Hi Mehmet</p>
+        <div class="row h-100">
+          <div class="col-xl-12 d-flex flex-column justify-content-center">
+            <div>
+              <p class="mb-0 text-white font-size-32 font-weight-bold">Hi Mehmet</p>
+              <p class="mb-0 font-size-12 text-light" style="opacity: .5;">Welcome back to the workspace, we missed you!</p>
+            </div>
+            <div class="w-50 mt-4">
+              <search-bar></search-bar>
+            </div>
           </div>
         </div>
       </div>
       <div class="col-xl-8 bg-white" style="border-radius: 1rem;">
         <div class="row">
-          <div class="col-xl-12">
+          <div class="col-xl-12 py-2">
             asd
           </div>
         </div>
@@ -18,3 +24,13 @@
     </div>
   </div>
 </template>
+
+<script>
+import SearchBar from "@/components/SearchBar"
+
+export default {
+  components: {
+    SearchBar
+  }
+}
+</script>
