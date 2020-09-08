af1e022999c5a9448f5204651da3f5ec767cdbb1
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-09-08 18:33:24 +0300

    Main layout created

---
+++ src/layouts/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="height: 100vh">
+    <div class="row h-100 bg-dark p-2 m-0">
+      <div class="col-xl-4">
+        <div class="row">
+          <div class="col-xl-12">
+            <p class="mb-0 text-white">Hi Mehmet</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-8 bg-white" style="border-radius: 1rem;">
+        <div class="row">
+          <div class="col-xl-12">
+            asd
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
