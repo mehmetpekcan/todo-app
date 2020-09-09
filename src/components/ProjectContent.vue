d1fad2d22fe92078fb8b73ffcd1fede1b2d384f9
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-09-09 03:12:18 +0300

    Add new task placement and view created

--- src/components/ProjectContent.vue
+++ src/components/ProjectContent.vue
@@ -1,25 +1,29 @@
 <template>
   <div style="overflow-y: scroll !important;">
-    <div class="title">
-      <p class="mb-1 font-size-16 text-dark text-uppercase font-weight-bold">{{ $route.params.name }}</p>
-      <p class="mb-0 font-size-12 w-50 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia vero odit voluptatum at sint, ipsa illum aliquid quis quam aperiam.</p>
+    <a-modal v-model="isNewTask" title="Add New Task" :footer="null">
+      <new-task />
+    </a-modal>
+
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="title">
+        <p class="mb-1 font-size-16 text-dark text-uppercase font-weight-bold">{{ $route.params.name }}</p>
+        <p class="mb-0 font-size-12 w-50 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia vero odit voluptatum at sint, ipsa illum aliquid quis quam aperiam.</p>
+      </div>
+      <div 
+        @click="newTask"
+        class="d-flex align-items-center bg-success text-white px-3 py-2 rounded"
+        style="box-shadow: 0 5px 10px -8px rgb(6, 127, 22); cursor: pointer;"
+        >
+        <i class="fas fa-plus mr-2" />
+        <span>Add New Task</span>
+      </div>
     </div>
     <div class="todos--wrapper mt-5">
       <div class="todos--title pb-3 border-bottom border-gray-3 d-flex justify-content-between align-items-center">
-        <p class="font-size-24 mb-0" style="font-weight: 600;">Today</p>
+        <p class="font-size-24 mb-0" style="font-weight: 600;">All</p>
       </div>
       <div class="todos--content-wrapper">
-        <div class="todo my-4" v-for="(item, key) in 6" :key="key">
-          <todo />
-        </div>
-      </div>
-    </div>
-    <div class="todos--wrapper mt-5">
-      <div class="todos--title pb-3 border-bottom border-gray-3 d-flex justify-content-between align-items-center">
-        <p class="font-size-24 mb-0" style="font-weight: 600;">Later</p>
-      </div>
-      <div class="todos--content-wrapper">
-        <div class="todo my-4" v-for="(item, key) in 6" :key="key">
+        <div class="todo my-4" v-for="(item, key) in 4" :key="key">
           <todo />
         </div>
       </div>
@@ -29,8 +33,19 @@
 
 <script>
 import Todo from "./Todo"
+import NewTask from "@/views/NewTask"
 
 export default {
-  components: { Todo },
+  data() {
+    return {
+      isNewTask: false
+    }
+  },
+  methods: {
+    newTask() {
+      this.isNewTask = true
+    }
+  },
+  components: { Todo, NewTask },
 }  
 </script>
---
+++ src/views/NewTask.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <a-form :form="form">
+    <a-form-item label="Description">
+      <a-textarea
+        :rows="4"
+        placeholder="Run for a while.."
+        v-decorator="['description', { rules: [{ required: true, message: 'Please enter a description' }]}]"
+        />
+    </a-form-item>
+    <a-form-item label="Tags (You can add your own tags just write)">
+      <a-select mode="tags" style="width: 100%" :token-separators="[',']" size="large">
+        <a-select-option v-for="(item, key) in ['First', 'Second', 'Third']" :key="key.toString()">
+          {{ item }} Priority
+        </a-select-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item label="Countdown">
+      <a-time-picker size="large" format="HH:mm" />
+      <a-tooltip>
+        <template #title>
+          You can set how much time will take the task to countdown.
+        </template>
+        <i class="fas fa-info-circle ml-3 font-size-16 text-dark" />
+      </a-tooltip>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" size="large" block>Create New Task</a-button>
+    </a-form-item>
+  </a-form>
+</div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  created() {
+    this.form = this.$form.createForm(this)
+  }
+}
+</script>
